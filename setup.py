618d9be439f5562d86bf483a4d00e04ba0597b01
ifilot <ivo@ivofilot.nl>
2020-12-30 19:27:59 +0100

    Creating Anaconda package

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 import subprocess
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+import os
+
+# specify paths on Windows to find compiler and libraries
+if os.name == 'nt':
+    # set path to cl executable
+    msvc_ver = "14.28.29333"
+    winkit_ver = "10.0.18362.0"
+    os.environ['PATH'] += r";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\%s\bin\Hostx64\x64" % msvc_ver
+    os.environ['PATH'] += r";C:\Program Files (x86)\Windows Kits\10\bin\%s\x64" % winkit_ver
+
+    # set path to include folders
+    os.environ['INCLUDE'] += r";C:\Program Files (x86)\Windows Kits\10\Include\%s\ucrt" % winkit_ver
+    os.environ['INCLUDE'] += r";C:\Program Files (x86)\Windows Kits\10\Include\%s\shared" % winkit_ver
+    os.environ['INCLUDE'] += r";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\%s\include" % msvc_ver
+
+    # some references to libraries
+    os.environ['LIB'] += r";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\%s\lib\x64" % msvc_ver
+    os.environ['LIB'] += r";C:\Program Files (x86)\Windows Kits\10\Lib\%s\um\x64" % winkit_ver
+    os.environ['LIB'] += r";C:\Program Files (x86)\Windows Kits\10\Lib\%s\ucrt\x64" % winkit_ver
+
+    # also specify some custom paths for libraries
+    os.environ['INCLUDE'] += r";D:\PROGRAMMING\LIBS\boost-1.74.0-win-x64\include"   # boost library
+    os.environ['INCLUDE'] += r";D:\PROGRAMMING\LIBS\eigen-3.3.9"                    # eigen3 linear algebra library
 
 def pkgconfig(package, kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
@@ -15,15 +38,16 @@ extension_kwargs = {
     'include_dirs': [],
 }
 
-# load eigen3
-extension_kwargs = pkgconfig('eigen3', extension_kwargs)
+if os.name == 'posix':
+    # load eigen3
+    extension_kwargs = pkgconfig('eigen3', extension_kwargs)
 
 ext_modules = [
     Extension(
         "pyqint.pyqint",
         ["pyqint/pyqint.pyx"],
-        extra_compile_args=['-fopenmp', '-O3'],
-        extra_link_args=['-fopenmp', '-O3'],
+        #extra_compile_args=['-fopenmp', '-O3'],
+        #extra_link_args=['-fopenmp', '-O3'],
         **extension_kwargs
     )
 ]
---
+++ tests/test_overlap.py
@@ -0,0 +1,45 @@
+import unittest
+from pyqint.pyqint import PyQInt, cgf, gto
+from copy import deepcopy
+import numpy as np
+
+class TestOverlap(unittest.TestCase):
+
+    def test_gto_overlap(self):
+        # construct integrator object
+        integrator = PyQInt()
+
+        # test GTO
+        gto1 = gto(0.154329, [0.0, 0.0, 0.0], 3.425251, 0, 0, 0)
+        gto2 = gto(0.535328, [0.0, 0.0, 0.0], 0.623914, 0, 0, 0)
+        gto3 = gto(0.444635, [0.0, 0.0, 0.0], 0.168855, 0, 0, 0)
+        overlap = integrator.overlap_gto(gto1, gto1)
+        result = 0.31055691838264465
+        np.testing.assert_almost_equal(overlap, result, 8)
+
+    def test_cgf_overlap(self):
+        integrator = PyQInt()
+
+        # build cgf for hydrogen seperated by 1.4 a.u.
+        cgf1 = cgf([0.0, 0.0, 0.0])
+
+        cgf1.add_gto(0.154329, 3.425251, 0, 0, 0)
+        cgf1.add_gto(0.535328, 0.623914, 0, 0, 0)
+        cgf1.add_gto(0.444635, 0.168855, 0, 0, 0)
+
+        cgf2 = deepcopy(cgf1)
+        cgf2.p[2] = 1.4
+
+        S = np.zeros((2,2))
+        S[0,0] = integrator.overlap(cgf1, cgf1)
+        S[0,1] = S[1,0] = integrator.overlap(cgf1, cgf2)
+        S[1,1] = integrator.overlap(cgf2, cgf2)
+
+        S11 = 1.0
+        S12 = 0.65931845
+        np.testing.assert_almost_equal(S[0,0], S11,86)
+        np.testing.assert_almost_equal(S[1,1], S11, 8)
+        np.testing.assert_almost_equal(S[0,1], S12, 8)
+
+if __name__ == '__main__':
+    unittest.main()
