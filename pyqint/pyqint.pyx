fa847178e55be4b3a35fbeab98e3ddafac196252
Ivo Filot <i.a.w.filot@tue.nl>
2020-12-31 14:17:28 +0100

    Implementing parallel evaluation of two-electron-integrals (#1)
    * Implementing parallel code execution
    
    * Adding parallel integral evaluation
    
    * Incrementing to version 5

--- pyqint/pyqint.pyx
+++ pyqint/pyqint.pyx
@@ -1,6 +1,11 @@
 # distutils: language = c++
 
 from .pyqint cimport Integrator, GTO
+from libc.stdint cimport uintptr_t
+
+import numpy as np
+from multiprocessing import Pool
+import tqdm
 
 class gto:
     """
@@ -26,10 +31,20 @@ class cgf:
         self.gtos.append(gto(c, self.p, alpha, l, m, n))
 
 cdef class PyQInt:
-    cdef Integrator integrator
+    cdef Integrator *integrator
+    integrator_uint = 0
 
     def __cinit__(self):
-        self.integrator = Integrator()
+        self.integrator = new Integrator()
+
+    def __dealloc__(self):
+        del self.integrator
+
+    def __getstate__(self):
+        return self.__class__
+
+    def __setstate__(self, d):
+        self.integrator = new Integrator()
 
     def overlap_gto(self, gto1, gto2):
 
@@ -155,5 +170,48 @@ cdef class PyQInt:
 
         return self.integrator.repulsion(c_cgf1, c_cgf2, c_cgf3, c_cgf4,)
 
+    def repulsion_contracted(self, cgfs):
+        return self.repulsion(cgfs[0], cgfs[1], cgfs[2], cgfs[3])
+
     def teindex(self, i, j, k, l):
         return self.integrator.teindex(i, j, k, l)
+
+    def build_integrals(self, cgfs, nuclei, npar=4, verbose=False):
+        # number of cgfs
+        N = len(cgfs)
+
+        # build empty matrices
+        S = np.zeros((N,N))
+        T = np.zeros((N,N))
+        V = np.zeros((N,N))
+        teint = np.multiply(np.ones(self.integrator.teindex(N,N,N,N)), -1.0)
+
+        for i, cgf1 in enumerate(cgfs):
+            for j, cgf2 in enumerate(cgfs):
+                S[i,j] = self.overlap(cgf1, cgf2)
+                T[i,j] = self.kinetic(cgf1, cgf2)
+
+                for nucleus in nuclei:
+                    V[i,j] += self.nuclear(cgf1, cgf2, nucleus[0], nucleus[1])
+
+        # build pool of jobs
+        jobs = [None] * (self.teindex(N-1,N-1,N-1,N-1)+1)
+        for i, cgf1 in enumerate(cgfs):
+            for j, cgf2 in enumerate(cgfs):
+                ij = i*(i+1)/2 + j
+                for k, cgf3 in enumerate(cgfs):
+                    for l, cgf4 in enumerate(cgfs):
+                        kl = k * (k+1)/2 + l
+                        if ij <= kl:
+                            idx = self.teindex(i,j,k,l)
+                            if teint[idx] < 0:
+                                jobs[idx] = cgfs[i],cgfs[j],cgfs[k],cgfs[l]
+
+        if verbose: # show a progress bar
+            with Pool(npar) as p:
+                teint = list(tqdm.tqdm(p.imap(func=self.repulsion_contracted, iterable=jobs), total=len(jobs)))
+        else:       # do not show a progress bar
+            with Pool(npar) as p:
+                teint = list(p.imap(func=self.repulsion_contracted, iterable=jobs))
+
+        return S, T, V, teint
