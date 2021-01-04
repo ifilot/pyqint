7c557b36c131454cbcd911dc73bb8d3acd90465d
ifilot <ivo@ivofilot.nl>
2021-01-04 09:12:16 +0100

    Incrementing version number

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pyqint',
-    version="0.6.0",
+    version="0.7.0",
     author="Ivo Filot",
     author_email="ivo@ivofilot.nl",
     description="Python package for evaluating integrals of Gaussian type orbitals in electronic structure calculations",
