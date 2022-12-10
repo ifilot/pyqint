f8f878ccab6aa0538a4bbdedeb8dd72250eb7f05
ifilot <ivo@ivofilot.nl>
2022-12-10 15:06:36 +0100

    Fixing bug and adding test

--- tests/test_hf.py
+++ tests/test_hf.py
@@ -22,8 +22,37 @@ class TestHF(unittest.TestCase):
         np.testing.assert_almost_equal(results['energy'], -73.21447132, 4)
 
         # verify that time statistics are being recorded
-        #self.assertTrue(results['time_stats']['integral_evaluation'] > 0)
-        #self.assertTrue(results['time_stats']['self_convergence'] > 0)
+        self.assertTrue(results['time_stats']['integral_evaluation'] > 0)
+        self.assertTrue(results['time_stats']['self_convergence'] > 0)
+
+    def testHartreeFockCH4Properties(self):
+        """
+        Test Hartree-Fock calculation on water using STO-3G basis set
+        """
+        mol = Molecule()
+        dist = 1.78/2
+        mol.add_atom('C', 0.0, 0.0, 0.0, unit='angstrom')
+        mol.add_atom('H', dist, dist, dist, unit='angstrom')
+        mol.add_atom('H', -dist, -dist, dist, unit='angstrom')
+        mol.add_atom('H', -dist, dist, -dist, unit='angstrom')
+        mol.add_atom('H', dist, -dist, -dist, unit='angstrom')
+
+        results = perform_hf(mol)
+
+        # check that orbital energies are correctly approximated
+        ans = np.array([-11.0707,
+                         -0.7392,
+                         -0.3752,
+                         -0.3752,
+                         -0.3752,
+                          0.2865,
+                          0.4092,
+                          0.4092,
+                          0.4092])
+        np.testing.assert_almost_equal(results['orbe'], ans, 4)
+
+        en = -39.35007843284954
+        np.testing.assert_almost_equal(results['energies'][-1], en, 4)
 
 def perform_hf(mol):
     results = HF().rhf(mol, 'sto3g')
