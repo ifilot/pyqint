ad4731687fae163684d86a0d02130e9176052a3a
ifilot <ivo@ivofilot.nl>
2023-05-14 20:22:59 +0200

    Fixing nose dependency

--- deploy/docker_test_github.sh
+++ deploy/docker_test_github.sh
@@ -4,7 +4,7 @@ set -e -u -x
 
 # Install packages and test
 for PYBIN in /opt/python/cp3{7,8,9,10,11}-*/bin; do
-    "${PYBIN}/python" -m pip install numpy pytest
+    "${PYBIN}/python" -m pip install numpy pytest nose
     "${PYBIN}/pip" install pyqint --no-index -f ./wheelhouse
     ("${PYBIN}/pytest" --verbose ./tests/*.py)
 done
