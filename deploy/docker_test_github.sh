a00b470069711a93715de132c504c17bbac79365
ifilot <ivo@ivofilot.nl>
2023-05-14 20:16:11 +0200

    Fixing bug in package name

--- deploy/docker_test_github.sh
+++ deploy/docker_test_github.sh
@@ -5,6 +5,6 @@ set -e -u -x
 # Install packages and test
 for PYBIN in /opt/python/cp3{7,8,9,10,11}-*/bin; do
     "${PYBIN}/python" -m pip install numpy pytest
-    "${PYBIN}/pip" install pytessel --no-index -f ./wheelhouse
+    "${PYBIN}/pip" install pyqint --no-index -f ./wheelhouse
     ("${PYBIN}/pytest" --verbose ./tests/*.py)
 done
