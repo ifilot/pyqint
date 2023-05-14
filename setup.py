205c1eadb834edb2bb638e07e1964d10d8926a58
ifilot <ivo@ivofilot.nl>
2023-05-14 19:44:36 +0200

    Initial commit Github Actions

--- setup.py
+++ setup.py
@@ -25,6 +25,12 @@ def find_windows_versions():
     Autofind the msvc and winkit versions
     """
     root = os.path.join('C:', os.sep,'Program Files', 'Microsoft Visual Studio', '2022', 'Community', 'VC', 'Tools', 'MSVC')
+
+    # for Gitlab actions, the above folder does not exist and this is communicated
+    # back by providing None as the result
+    if not os.path.exists(root):
+        return None, None
+
     for file in os.listdir(root):
         if os.path.isdir(os.path.join(root, file)):
             msvcver = file
@@ -57,6 +63,27 @@ if os.name == 'nt':
     os.environ['INCLUDE'] += r";D:\PROGRAMMING\LIBS\boost-1.74.0-win-x64\include"   # boost library
     os.environ['INCLUDE'] += r";C:\PROGRAMMING\LIBS\eigen-3.3.9"                    # eigen3 linear algebra library
     os.environ['INCLUDE'] += r";D:\PROGRAMMING\LIBS\eigen-3.3.9"                    # eigen3 linear algebra library
+else:
+    # if msvc_ver and winkit_ver are set to None, this means we are working on Gitlab Actions
+    # which requires the paths to be set differently; note that the glm-0.9.9.8.zip file has
+    # a different root path than the glm-0.9.9.8.tar.gz file.
+    os.environ['INCLUDE'] += r";" + os.environ['GITHUB_WORKSPACE'] + r"\glm"
+
+    # re-order paths to ensure that the MSVC toolchain is in front; this needs to be done
+    # because the Git bin folder precedes the MSVC bin folder, resulting in the wrong link.exe
+    # executable to be used in the linking step
+    paths = os.environ['PATH'].split(";")
+    newpaths = []
+    for path in paths:
+        if "Microsoft Visual Studio" in path:
+            newpaths = [path] + newpaths
+        else:
+            newpaths.append(path)
+    os.environ['PATH'] = ";".join(newpaths)
+
+    # also specify some custom paths for libraries
+    # os.environ['INCLUDE'] += r";C:\PROGRAMMING\LIBS\boost-1.74.0-win-x64\include"   # boost library
+    # os.environ['INCLUDE'] += r";D:\PROGRAMMING\LIBS\eigen-3.3.9"                    # eigen3 linear algebra library
 
 # specify compilation instructions for other platforms
 if os.name == 'posix' and sys.platform != 'darwin':
