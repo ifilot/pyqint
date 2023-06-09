b02b9a53508c2f9b5832827c6f1489d6e82b238a
ifilot <ivo@ivofilot.nl>
2023-06-09 20:00:31 +0200

    Redesigning README.md

--- README.md
+++ README.md
@@ -6,41 +6,31 @@
 [![PyPI](https://img.shields.io/pypi/v/pyqint?style=flat-square)](https://pypi.org/project/pyqint/)
 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
+__Table of Contents__
+
+* [Purpose](#purpose)
+* [Documentation](#documentation)
+* [Examples](#documentation)
+    - [Contour plots of the molecular orbitals of CO](#contour-plots-of-the-molecular-orbitals-of-co)
+
 ## Purpose
 
 PyQInt is a Python package for calculating one- and two-electron integrals as
 encountered in electronic structure calculations. Since integral evaluation can
 be quite computationally intensive, they are programmed in C++ and connected to
-Python using Cython.
+Python using Cython. PyQInt offers additional features such as performed restricted **Hartree-Fock
+calculations**, calculation of **Crystal Orbital Hamilton Population** coefficients
+and constructing localized orbitals using the **Boys-Foster** method.
 
 ## Documentation
 
 PyQInt comes with detailed documentation and examples, which can be found
 at https://pyqint.imc-tue.nl.
 
-## Installation
+## Examples
 
-### Anaconda
+### Contour plots of the molecular orbitals of CO
 
-[![Anaconda-Server Badge](https://anaconda.org/ifilot/pyqint/badges/version.svg)](https://anaconda.org/ifilot/pyqint)
-[![Anaconda-Server Badge](https://anaconda.org/ifilot/pyqint/badges/platforms.svg)](https://anaconda.org/ifilot/pyqint)
-[![Anaconda-Server Badge](https://anaconda.org/ifilot/pyqint/badges/downloads.svg)](https://anaconda.org/ifilot/pyqint)
+Below, the molecular orbitals for CO using a `sto-3g` basis set are shown.
 
-Open Anaconda prompt and type
-
-```
-conda install -c ifilot pyqint
-```
-
-### PyPi
-
-[![PyPI](https://img.shields.io/pypi/v/pyqint?color=green&style=flat-square)](https://pypi.org/project/pyqint/)
-[![PyPI - Downloads](https://img.shields.io/pypi/dm/pypi?style=flat-square)](https://pypi.org/project/pyqint/)
-![PyPI - Python Version](https://img.shields.io/pypi/pyversions/pyqint?style=flat-square)
-
-
-Open a terminal and type
-
-```
-pip install pyqint
-```
+![Molecular orbitals of CO](img/co.jpg)
---
+++ examples/molecular_orbitals_co.py
@@ -0,0 +1,80 @@
+from pyqint import Molecule, HF, PyQInt
+import numpy as np
+import matplotlib.pyplot as plt
+
+#
+# Plot the isosurfaces for the CO molecule
+#
+
+def main():
+    # calculate sto-3g coefficients for co
+    result = calculate_co(1.145414)
+    energies = result['orbe']
+    coeff = result['orbc']
+        
+    fig, ax = plt.subplots(2, 5, dpi=144, figsize=(12,5))           
+    for i,(chi,e) in enumerate(zip(coeff.transpose(), energies)):
+        res, x, y = build_isosurface(result['cgfs'], chi, sz=3, plane='xz')
+        vmax = np.max(np.abs(res))
+        vmin = -vmax
+        ax[i//5,i%5].contourf(x, y, res, levels=15, cmap='PiYG',
+                              vmin=vmin, vmax=vmax)
+        ax[i//5,i%5].contour(x, y, res, levels=15, colors='black',
+                             vmin=vmin, vmax=vmax)
+        ax[i//5,i%5].set_xlabel('x [Bohr]')
+        ax[i//5,i%5].set_ylabel('z [Bohr]')
+        ax[i//5,i%5].set_title('Energy = %.4f Ht' % e)
+        ax[i//5,i%5].grid(linestyle='--', color='black', alpha=0.5)
+        
+    plt.tight_layout()
+    plt.savefig('co.jpg')
+
+def optimize_co(d):
+    """
+    Optimization function for scipy.optimize.minimize
+    """
+    mol = Molecule()
+    mol.add_atom('C', 0.0, 0.0, -d[0]/2, unit='angstrom')
+    mol.add_atom('O', 0.0, 0.0,  d[0]/2, unit='angstrom')
+    
+    result = HF().rhf(mol, 'sto3g')
+    
+    return result['energy']
+
+def calculate_co(d):
+    """
+    Full function for evaluation
+    """
+    mol = Molecule()
+    mol.add_atom('C', 0.0, 0.0, -d/2, unit='angstrom')
+    mol.add_atom('O', 0.0, 0.0,  d/2, unit='angstrom')
+    
+    result = HF().rhf(mol, 'sto3g')
+    
+    return result
+
+def build_isosurface(cgfs, coeff, sz=2, npts=50, plane='xy'):
+    integrator = PyQInt()
+    
+    # build grid
+    x = np.linspace(-sz, sz, 50)
+    y = np.linspace(-sz, sz, 50)
+    xx, yy = np.meshgrid(x,y)
+    zz = np.zeros(len(x) * len(y))
+    
+    if plane == 'xy':
+        points = [xx.flatten(), yy.flatten(), zz]
+    elif plane == 'xz':
+        points = [xx.flatten(), zz, yy.flatten()]
+    elif plane == 'yz':
+        points = [zz, xx.flatten(), yy.flatten()]
+    else:
+        raise Exception('Unknown plane: %s' % plane)
+    
+    grid = np.vstack(points).reshape(3,-1).T
+    res = integrator.plot_wavefunction(grid, coeff, cgfs).reshape((len(y), len(x)))
+    
+    return res, x, y
+
+if __name__ == '__main__':
+    main()
