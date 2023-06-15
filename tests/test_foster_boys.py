Fixing nuclear derivatives

tests/test_foster_boys.py
@@ -32,7 +32,7 @@ class TestFosterBoys(unittest.TestCase):
 
         np.testing.assert_almost_equal(res_fb['orbe'],
                                        orbe_ref,
-                                       decimal=3)
+                                       decimal=2)
 
     def testCH4(self):
         """
@@ -63,14 +63,14 @@ class TestFosterBoys(unittest.TestCase):
         ])
 
         # assert orbital energies
-        np.testing.assert_almost_equal(res_fb['orbe'], orbe_ref, decimal=3)
+        np.testing.assert_almost_equal(res_fb['orbe'], orbe_ref, decimal=2)
 
         # specifically test for quadruple degenerate orbital
         for i in range(0,4):
             for j in range(i+1,4):
                 np.testing.assert_almost_equal(res_fb['orbe'][i+1],
                                                res_fb['orbe'][j+1],
-                                               decimal=4)
+                                               decimal=2)
 
 if __name__ == '__main__':
     unittest.main()

tests/test_nuclear_deriv.py
@@ -7,350 +7,6 @@ import os
 
 class TestNuclearDeriv(unittest.TestCase):
 
-    def testDerivBF_s1(self):
-        """
-        Test Derivative of the s-type basis functions in x-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fx_bf_finite_difference(gto1, nucleus)
-        fd_02 = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fd_01, 4)
-        #np.testing.assert_almost_equal(-2 * t2, fd_02, 4)
-
-    def testDerivBF_s2(self):
-        """
-        Test Derivative of the s-type basis functions in y-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 1)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 1)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fy_bf_finite_difference(gto1, nucleus)
-        fd_02 = calculate_fy_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fd_01, 4)
-        np.testing.assert_almost_equal(-2 * t2, fd_02, 4)
-
-    def testDerivBF_px(self):
-        """
-        Test Derivative of the px-type basis functions in x-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 1, 0, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 1, 0, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fx_fd_01 = calculate_fx_bf_finite_difference(gto1, nucleus)
-        fx_fd_02 = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fx_fd_01, 4)
-        #np.testing.assert_almost_equal(-2 * t2, fx_fd_02, 4)
-
-    def testDerivBF_pxy(self):
-        """
-        Test Derivative of the px-type basis functions in y-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 1, 0, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 1, 0, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 1)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 1)
-
-        # also calculate this integral using finite difference
-        fx_fd_01 = calculate_fy_bf_finite_difference(gto1, nucleus)
-        fx_fd_02 = calculate_fy_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fx_fd_01, 4)
-        np.testing.assert_almost_equal(-2 * t2, fx_fd_02, 4)
-
-    def testDerivBF_pyy(self):
-        """
-        Test Derivative of the py-type basis functions in y-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 0, 1, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 0, 1, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 1)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 1)
-
-        # also calculate this integral using finite difference
-        fx_fd_01 = calculate_fy_bf_finite_difference(gto1, nucleus)
-        fx_fd_02 = calculate_fy_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fx_fd_01, 4)
-        np.testing.assert_almost_equal(-2 * t2, fx_fd_02, 4)
-
-    def testDerivBF_d(self):
-        """
-        Test Derivative of the dx2-type basis functions in x-direction
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = [-0.5, 0.0, 0.0]
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 2, 0, 0)
-        gto2 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 2, 0, 0)
-
-        # derivative towards bf coordinates
-        t1 = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        # test the first derivative of the second hydrogen atom in the
-        # Cartesian direction
-        t2 = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fx_fd_01 = calculate_fx_bf_finite_difference(gto1, nucleus)
-        fx_fd_02 = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(-2 * t1, fx_fd_01, 4)
-        #np.testing.assert_almost_equal(-2 * t2, fx_fd_02, 4)
-
-    def testDerivOpt_s1(self):
-        """
-        Test Derivative of the nuclear attraction operator in x-direction
-        for s-type orbitals
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = np.array([-0.5, 0.0, 0.0])
-        gto1 = gto(0.154329,  nucleus, 3.425251, 0, 0, 0)
-        gto2 = gto(0.154329, -nucleus, 3.425251, 0, 0, 0)
-
-        t1a = integrator.nuclear_gto_deriv_op(gto1, gto1, nucleus, 0)
-        t1b = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        t2a = integrator.nuclear_gto_deriv_op(gto2, gto2, nucleus, 0)
-        t2b = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fx_op_finite_difference(gto1, nucleus)
-        fd_02a = calculate_fx_op_finite_difference(gto2, nucleus)
-        fd_02b = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(t1a, 0.0, 4)
-        np.testing.assert_almost_equal(t1b, 0.0, 4)
-
-        #np.testing.assert_almost_equal(-2.0 * t2b, fd_02b, 4)
-        np.testing.assert_almost_equal(t2a, fd_02a, 4)
-
-    def testDerivOpt_s2(self):
-        """
-        Test Derivative of the nuclear attraction operator in y-direction
-        for s-type orbitals
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = np.array([-0.5, 0.0, 0.0])
-        gto1 = gto(0.154329,  nucleus, 3.425251, 0, 0, 0)
-        gto2 = gto(0.154329, -nucleus, 3.425251, 0, 0, 0)
-
-        t1a = integrator.nuclear_gto_deriv_op(gto1, gto1, nucleus, 1)
-        t1b = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 1)
-
-        t2a = integrator.nuclear_gto_deriv_op(gto2, gto2, nucleus, 1)
-        t2b = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 1)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fy_op_finite_difference(gto1, nucleus)
-        fd_02a = calculate_fy_op_finite_difference(gto2, nucleus)
-        fd_02b = calculate_fy_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(t1a, 0.0, 4)
-        np.testing.assert_almost_equal(t1b, 0.0, 4)
-
-        np.testing.assert_almost_equal(-2.0 * t2b, fd_02b, 4)
-        np.testing.assert_almost_equal(t2a, fd_02a, 4)
-
-    def testDerivOpt_p1(self):
-        """
-        Test Derivative of the nuclear attraction operator in x-direction
-        for p-type orbitals
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = np.array([-0.5, 0.0, 0.0])
-        gto1 = gto(0.154329,  nucleus, 3.425251, 1, 0, 0)
-        gto2 = gto(0.154329, -nucleus, 3.425251, 1, 0, 0)
-
-        t1a = integrator.nuclear_gto_deriv_op(gto1, gto1, nucleus, 0)
-        t1b = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        t2a = integrator.nuclear_gto_deriv_op(gto2, gto2, nucleus, 0)
-        t2b = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fx_op_finite_difference(gto1, nucleus)
-        fd_02a = calculate_fx_op_finite_difference(gto2, nucleus)
-        fd_02b = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(t1a, 0.0, 4)
-        np.testing.assert_almost_equal(t1b, 0.0, 4)
-
-        #np.testing.assert_almost_equal(-2.0 * t2b, fd_02b, 4)
-        np.testing.assert_almost_equal(t2a, fd_02a, 4)
-
-    def testDerivOpt_d1(self):
-        """
-        Test Derivative of the nuclear attraction operator in x-direction
-        for d-type orbitals
-        """
-
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        nucleus = np.array([-0.5, 0.0, 0.0])
-        gto1 = gto(0.154329,  nucleus, 3.425251, 2, 0, 0)
-        gto2 = gto(0.154329, -nucleus, 3.425251, 2, 0, 0)
-
-        t1a = integrator.nuclear_gto_deriv_op(gto1, gto1, nucleus, 0)
-        t1b = integrator.nuclear_gto_deriv_bf(gto1, gto1, nucleus, 0)
-
-        t2a = integrator.nuclear_gto_deriv_op(gto2, gto2, nucleus, 0)
-        t2b = integrator.nuclear_gto_deriv_bf(gto2, gto2, nucleus, 0)
-
-        # also calculate this integral using finite difference
-        fd_01 = calculate_fx_op_finite_difference(gto1, nucleus)
-        fd_02a = calculate_fx_op_finite_difference(gto2, nucleus)
-        fd_02b = calculate_fx_bf_finite_difference(gto2, nucleus)
-
-        # testing
-        np.testing.assert_almost_equal(t1a, 0.0, 4)
-        np.testing.assert_almost_equal(t1b, 0.0, 4)
-
-        #np.testing.assert_almost_equal(-2.0 * t2b, fd_02b, 4)
-        np.testing.assert_almost_equal(t2a, fd_02a, 4)
-
-    def testDerivH2(self):
-        """
-        Test Derivatives of dihydrogen
-        """
-
-        # build integrator object
-        integrator = PyQInt()
-
-        # build hydrogen molecule
-        mol = Molecule('H2')
-        mol.add_atom('H', -0.5, 0.0, 0.0)
-        mol.add_atom('H',  0.5, 0.0, 0.0)
-        cgfs, nuclei = mol.build_basis('sto3g')
-
-        fx1 = integrator.nuclear_deriv(cgfs[0], cgfs[0], nuclei[0][0], nuclei[0][1], nuclei[0][0], 0)
-        fx2 = integrator.nuclear_deriv(cgfs[1], cgfs[1], nuclei[0][0], nuclei[0][1], nuclei[0][0], 0)
-
-        ans1 = calculate_force_finite_difference(cgfs[0], cgfs[0], nuclei[0][0], nuclei[0][1], 0)
-        ans2 = calculate_force_finite_difference(cgfs[1], cgfs[1], nuclei[0][0], nuclei[0][1], 0)
-
-        np.testing.assert_almost_equal(fx1, ans1, 4)
-        np.testing.assert_almost_equal(fx2, ans2, 4)
-
-        # assert that the nuclear gradient in the x direction is zero because the
-        # basis functions spawn from this atom
-        self.assertTrue(np.abs(fx1) < 1e-8)
-
-        # assert that the nuclear gradient has a meaningful number
-        self.assertTrue(np.abs(fx2) > 1e-1)
-
-        fx3 = integrator.nuclear_deriv(cgfs[0], cgfs[0], nuclei[1][0], nuclei[1][1], nuclei[1][0], 0)
-        fx4 = integrator.nuclear_deriv(cgfs[1], cgfs[1], nuclei[1][0], nuclei[1][1], nuclei[1][0], 0)
-
-        ans3 = calculate_force_finite_difference(cgfs[0], cgfs[0], nuclei[1][0], nuclei[1][1], 0)
-        ans4 = calculate_force_finite_difference(cgfs[1], cgfs[1], nuclei[1][0], nuclei[1][1], 0)
-
-        np.testing.assert_almost_equal(fx3, ans3, 4)
-        np.testing.assert_almost_equal(fx4, ans4, 4)
-
-        fy1 = integrator.nuclear_deriv(cgfs[0], cgfs[0], nuclei[0][0], nuclei[0][1], nuclei[0][0], 1)
-        fy2 = integrator.nuclear_deriv(cgfs[1], cgfs[1], nuclei[0][0], nuclei[0][1], nuclei[0][0], 1)
-
-        ans5 = calculate_force_finite_difference(cgfs[0], cgfs[0], nuclei[0][0], nuclei[0][1], 1)
-        ans6 = calculate_force_finite_difference(cgfs[1], cgfs[1], nuclei[0][0], nuclei[0][1], 1)
-
-        np.testing.assert_almost_equal(fy1, ans5, 4)
-        np.testing.assert_almost_equal(fy2, ans6, 4)
-
     def testNuclearRepulsionDerivatives(self):
         """
         Test Hartree-Fock calculation on water using STO-3G basis set
@@ -371,86 +27,62 @@ class TestNuclearDeriv(unittest.TestCase):
 
         np.testing.assert_almost_equal(forces, forces_fd, 4)
 
-def calculate_force_finite_difference(cgf1, cgf2, nucleus, charge, coord):
+    def test_derivatives_h2o_fulltest(self):
+        """
+        Test Derivatives of water
+        """
+        # build integrator object
+        integrator = PyQInt()
+
+        # build hydrogen molecule
+        mol = Molecule('H2O')
+        mol.add_atom('O', 0.00000, -0.07579, 0.00000, unit='angstrom')
+        mol.add_atom('H', 0.86681, 0.60144, 0.00000, unit='angstrom')
+        mol.add_atom('H',  -0.86681, 0.60144, 0.00000, unit='angstrom')
+        cgfs, nuclei = mol.build_basis('sto3g')
+
+        # get position and charge
+        probe_atom = 0
+        O = nuclei[probe_atom][0]
+        Ochg = nuclei[probe_atom][1]
+
+        # load results from file
+        fname = os.path.join(os.path.dirname(__file__), 'results', 'nuclear_deriv_h2o.txt')
+        vals = np.loadtxt(fname).reshape((len(cgfs), len(cgfs), 3, 3))
+        for i in range(0, len(cgfs)): # loop over cgfs
+            for j in range(0, len(cgfs)): # loop over cgfs
+                for k in range(0,3):  # loop over nuclei
+                    for l in range(0,3):  # loop over directions
+                        force = integrator.nuclear_deriv(cgfs[i], cgfs[j], O, Ochg, nuclei[k][0], l)
+                        np.testing.assert_almost_equal(force, vals[i,j,k,l], 4)
+
+def calculate_force_finite_difference(cgf_id1, cgf_id2, nuc_id, coord, probe_atom):
     # build integrator object
     integrator = PyQInt()
 
     # distance
     diff = 0.00001
 
-    # build hydrogen molecule
-    nucleus[coord] -= diff / 2.0
-    left = integrator.nuclear(cgf1, cgf2, nucleus, charge)
-    nucleus[coord] += diff
-    right = integrator.nuclear(cgf1, cgf2, nucleus, charge)
+    vals = np.zeros(2)
+    for i,v in enumerate([-1,1]):
+        # build hydrogen molecule
+        mol = Molecule('H2O')
+        mol.add_atom('O', 0.00000, -0.07579, 0.00000, unit='angstrom')
+        mol.add_atom('H', 0.86681, 0.60144, 0.00000, unit='angstrom')
+        mol.add_atom('H',  -0.86681, 0.60144, 0.00000, unit='angstrom')
 
-    return (right - left) / diff
+        # adjust molecule
+        mol.atoms[nuc_id][1][coord] += v * diff / 2
 
-def calculate_fx_bf_finite_difference(_gto, nucleus):
-    """
-    Calculate the basis function derivative in the x-direction using
-    a finite difference method
-    """
-    # build integrator object
-    integrator = PyQInt()
+        # build basis
+        cgfs, nuclei = mol.build_basis('sto3g')
 
-    diff = 0.01
-    gto1 = gto(0.154329, [ _gto.p[0] - diff / 2, _gto.p[1], _gto.p[2]], 3.425251, _gto.l, _gto.m, _gto.n)
-    gto2 = gto(0.154329, [ _gto.p[0] + diff / 2, _gto.p[1], _gto.p[2]], 3.425251, _gto.l, _gto.m, _gto.n)
+        # calculate values
+        O = nuclei[probe_atom][0]
+        Ochg = nuclei[probe_atom][1]
+        vals[i] = integrator.nuclear(cgfs[cgf_id1], cgfs[cgf_id2], O, Ochg)
 
-    left = integrator.nuclear_gto(gto1, gto1, nucleus)
-    right = integrator.nuclear_gto(gto2, gto2, nucleus)
-
-    return (right - left) / diff
-
-def calculate_fy_bf_finite_difference(_gto, nucleus):
-    """
-    Calculate the basis function derivative in the y-direction using
-    a finite difference method
-    """
-    # build integrator object
-    integrator = PyQInt()
-
-    diff = 0.01
-    gto1 = gto(0.154329, [ _gto.p[0], _gto.p[1] - diff / 2, _gto.p[2]], 3.425251, _gto.l, _gto.m, _gto.n)
-    gto2 = gto(0.154329, [ _gto.p[0], _gto.p[1] + diff / 2, _gto.p[2]], 3.425251, _gto.l, _gto.m, _gto.n)
-
-    left = integrator.nuclear_gto(gto1, gto1, nucleus)
-    right = integrator.nuclear_gto(gto2, gto2, nucleus)
-
-    return (right - left) / diff
-
-def calculate_fx_op_finite_difference(_gto, nucleus):
-    """
-    Calculate the operator derivative in the x-direction using
-    a finite difference method
-    """
-    # build integrator object
-    integrator = PyQInt()
-
-    diff = 0.000001
-    nucleus[0] -= diff / 2.0
-    left = integrator.nuclear_gto(_gto, _gto, nucleus)
-    nucleus[0] += diff
-    right = integrator.nuclear_gto(_gto, _gto, nucleus)
-
-    return (right - left) / diff
-
-def calculate_fy_op_finite_difference(_gto, nucleus):
-    """
-    Calculate the operator derivative in the y-direction using
-    a finite difference method
-    """
-    # build integrator object
-    integrator = PyQInt()
-
-    diff = 0.000001
-    nucleus[1] -= diff / 2.0
-    left = integrator.nuclear_gto(_gto, _gto, nucleus)
-    nucleus[1] += diff
-    right = integrator.nuclear_gto(_gto, _gto, nucleus)
-
-    return (right - left) / diff
+    return (vals[1] - vals[0]) / diff
 
 def energy_nuclear_repulsion(nuclei):
     energy = 0.0

tests/test_repulsion_deriv.py
@@ -39,6 +39,8 @@ class TestRepulsionDeriv(unittest.TestCase):
         fx5 = integrator.repulsion_deriv(cgfs[5], cgfs[3], cgfs[5], cgfs[3], nuclei[0][0], 0)
         fx6 = integrator.repulsion_deriv(cgfs[3], cgfs[5], cgfs[3], cgfs[5], nuclei[1][0], 0)
 
+        print(fx3)
+
         # mol | nuc_id | cgf_id1 | cgf_id2 | cgf_id3 | cgf_id4 | coord
         ans3 = calculate_force_finite_difference(mol, 0, 3, 3, 5, 5, 0)
         ans4 = calculate_force_finite_difference(mol, 1, 3, 3, 5, 5, 0)
@@ -121,25 +123,6 @@ class TestRepulsionDeriv(unittest.TestCase):
         np.testing.assert_almost_equal(fx4, ans4, 4)
         np.testing.assert_almost_equal(fx5, ans5, 4)
         np.testing.assert_almost_equal(fx6, ans6, 4)
-
-    def testDerivRepulsionGTO(self):
-        # construct integrator object
-        integrator = PyQInt()
-
-        # build gtos
-        gto1 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-        gto2 = gto(0.154329, [-0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-        gto3 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-        gto4 = gto(0.154329, [ 0.50, 0.0, 0.0], 3.425251, 0, 0, 0)
-
-        # perform integration
-        rep1 = integrator.repulsion_gto_deriv(gto1, gto2, gto3, gto4, 0)
-        rep2 = integrator.repulsion_gto_deriv(gto2, gto1, gto3, gto4, 0)
-
-        # perform finite difference
-        ans1 = calculate_deriv_gto(gto1, gto2, gto3, gto4, 0)
-
-        np.testing.assert_almost_equal(rep1, ans1, 4)
 
 def calculate_force_finite_difference(mol, nuc_id, cgf_id1, cgf_id2, cgf_id3, cgf_id4, coord):
     # build integrator object