Fixing derivatives

tests/test_geometry_optimization.py
@@ -75,7 +75,7 @@ class TestGeometryOptimization(unittest.TestCase):
         mol.add_atom('H', dist, -dist, -dist, unit='angstrom')
 
         res = GeometryOptimization(verbose=False).run(mol, 'sto3g')
-        np.testing.assert_almost_equal(res['opt'].fun, -39.726905821)
+        np.testing.assert_almost_equal(res['opt'].fun, -39.72691085946399)
 
         self.assertEqual(len(res['energies']), len(res['forces']))
         self.assertEqual(len(res['energies']), len(res['coordinates']))
@@ -102,7 +102,7 @@ class TestGeometryOptimization(unittest.TestCase):
         mol.add_atom('H', 0.0,  0.8580158822, 0.5085242828, unit='angstrom')
 
         res = GeometryOptimization(verbose=False).run(mol, 'sto3g')
-        np.testing.assert_almost_equal(res['opt'].fun, -74.9658999323)
+        np.testing.assert_almost_equal(res['opt'].fun, -74.96590347517174)
 
         self.assertEqual(len(res['energies']), len(res['forces']))
         self.assertEqual(len(res['energies']), len(res['coordinates']))
@@ -132,7 +132,7 @@ class TestGeometryOptimization(unittest.TestCase):
         mol.add_atom('H',  1.1288875372, -0.9156191261 ,0.1000000000, unit='angstrom')
 
         res = GeometryOptimization(verbose=False).run(mol, 'sto3g')
-        np.testing.assert_almost_equal(res['opt'].fun, -77.073956517)
+        np.testing.assert_almost_equal(res['opt'].fun, -77.07396213047552)
 
         self.assertEqual(len(res['energies']), len(res['forces']))
         self.assertEqual(len(res['energies']), len(res['coordinates']))