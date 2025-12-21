353d7691edc2065049cdff291247163c1ff7e61e
Ivo Filot <ivo@ivofilot.nl>
2025-12-21 07:10:45 +0100

    Fix version retrieval for pyqint package

--- pyqint/_version.py
+++ pyqint/_version.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("pymodia")
+    __version__ = version("pyqint")
 except PackageNotFoundError:
     __version__ = "0.0.0"  # optional fallback
