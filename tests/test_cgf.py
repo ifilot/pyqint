fix: missing normalization constant for CGFs

tests/test_cgf.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 
 class TestCGF(unittest.TestCase):
-
     def testFunctionsCGF(self):
         """
         Test getting amplitude from CGF
@@ -27,8 +26,20 @@ class TestCGF(unittest.TestCase):
             coords.append([x, x, x])
 
         # results
-        ans = [6.2825e-01, 7.1229e-02, 6.8672e-03, 3.0000e-04, 3.7662e-06,
-               1.3536e-08, 1.3927e-11, 4.1021e-15, 3.4591e-19, 8.3505e-24]
+        ans = np.array(
+            [
+                6.282468937e-01,
+                7.122864446e-02,
+                6.867199515e-03,
+                3.000012405e-04,
+                3.766188787e-06,
+                1.353554025e-08,
+                1.392653953e-11,
+                4.102087793e-15,
+                3.459081532e-19,
+                8.350466908e-24,
+            ]
+        )
 
         # test for each coord
         amps = []
@@ -36,7 +47,7 @@ class TestCGF(unittest.TestCase):
             amp = cgfs[0].get_amp(coord)
             amps.append(amp)
 
-        np.testing.assert_almost_equal(amps, ans, 4)
+        np.testing.assert_almost_equal(amps, ans, 9)
 
     def testPlotGrid(self):
         """
@@ -78,12 +89,94 @@ class TestCGF(unittest.TestCase):
                 orb.add_spherical_gto(1.0, 1.0, l, m)
                 basis_functions.append(orb)
         # build overlap matrix
-        om = np.zeros((len(basis_functions), len(basis_functions)))
+        nbf = len(basis_functions)
+        S = np.zeros((nbf, nbf))
         integrator = PyQInt()
-        for i,orb1 in enumerate(basis_functions):
-            for j,orb2 in enumerate(basis_functions):
-                om[i,j] = integrator.overlap(orb1, orb2)
-        np.testing.assert_almost_equal(om, np.eye(om.shape[0]), 6)
+        for i, orb1 in enumerate(basis_functions):
+            for j, orb2 in enumerate(basis_functions):
+                S[i,j] = integrator.overlap(orb1, orb2)
+        np.testing.assert_almost_equal(S, np.eye(S.shape[0]), 6)
+
+    def testSinglePrimitiveNormalization(self):
+        """
+        Test that a single primitive GTO has self-overlap of 1.0
+        """
+        integrator = PyQInt()
+        p0 = [0.0, 0.0, 0.0]
+
+        # Test various angular momenta with different exponents
+        test_cases = [
+            # (alpha, l, m, n)
+            (1.0, 0, 0, 0),   # s orbital
+            (0.5, 1, 0, 0),   # px orbital
+            (2.0, 0, 1, 0),   # py orbital
+            (1.5, 0, 0, 1),   # pz orbital
+            (1.0, 2, 0, 0),   # dxx orbital
+            (1.0, 1, 1, 0),   # dxy orbital
+            (0.8, 0, 0, 2),   # dzz orbital
+        ]
+
+        for alpha, l, m, n in test_cases:
+            cgf = CGF(p0)
+            cgf.add_gto(1.0, alpha, l, m, n)
+            S = integrator.overlap(cgf, cgf)
+            self.assertAlmostEqual(S, 1.0, places=10,
+                msg=f"Single primitive ({l},{m},{n}) with alpha={alpha} failed: S={S}")
+
+    def testContractedBasisNormalization(self):
+        """
+        Test that contracted basis functions (STO-3G) have self-overlap of 1.0
+        This verifies the contraction normalization is applied correctly.
+        """
+        integrator = PyQInt()
+
+        # Build H2 molecule with STO-3G basis
+        mol = Molecule()
+        mol.add_atom('H', 0.0, 0.0, 0.0)
+        mol.add_atom('H', 1.4, 0.0, 0.0)  # ~0.74 Angstrom bond length
+        cgfs, _ = mol.build_basis('sto3g')
+
+        # All diagonal elements of overlap matrix should be 1.0
+        for i, cgf in enumerate(cgfs):
+            S_ii = integrator.overlap(cgf, cgf)
+            self.assertAlmostEqual(S_ii, 1.0, places=10,
+                msg=f"Contracted basis function {i} has self-overlap {S_ii}")
+
+    def testOverlapMatrixSymmetryAndDiagonal(self):
+        """
+        Test overlap matrix properties for a multi-atom system:
+        1. Diagonal elements should be 1.0 (normalisation)
+        2. Matrix should be symmetric
+        3. Off-diagonal elements should be < 1.0
+        """
+        integrator = PyQInt()
+
+        # Build water molecule with STO-3G basis
+        mol = Molecule('H2O')
+        mol.add_atom('O', 0.0, 0.0, 0.0)
+        mol.add_atom('H', 0.7570, 0.5860, 0.0)
+        mol.add_atom('H', -0.7570, 0.5860, 0.0)
+        cgfs, nuclei = mol.build_basis('sto3g')
+
+        # Build full overlap matrix
+        nbf = len(cgfs)
+        S = np.zeros((nbf, nbf))
+        for i in range(nbf):
+            for j in range(nbf):
+                S[i, j] = integrator.overlap(cgfs[i], cgfs[j])
+
+        # Test 1: Diagonal elements should be 1.0
+        np.testing.assert_almost_equal(np.diag(S), np.ones(nbf), decimal=10,
+            err_msg="Diagonal elements of overlap matrix are not 1.0")
+
+        # Test 2: Matrix should be symmetric
+        np.testing.assert_almost_equal(S, S.T, decimal=10,
+            err_msg="Overlap matrix is not symmetric")
+
+        # Test 3: Off-diagonal elements should have magnitude < 1.0
+        off_diag = S - np.diag(np.diag(S))
+        self.assertTrue(np.all(np.abs(off_diag) < 1.0),
+            msg="Off-diagonal overlap elements should have magnitude < 1.0")
 
 if __name__ == '__main__':
     unittest.main()

tests/test_custom_basis_set.py
@@ -3,11 +3,11 @@ from pyqint import Molecule, CGF, HF, MoleculeBuilder
 import numpy as np
 
 class TestCustomBasisSet(unittest.TestCase):
-    
+
     def test_custom_basis_set_h2(self):
         mol = Molecule()
         mol.add_atom('H', 0.0000, 0.0000, 0.3561150187, unit='angstrom')
-        mol.add_atom('H', 0.0000, 0.0000, -0.3561150187, unit='angstrom')        
+        mol.add_atom('H', 0.0000, 0.0000, -0.3561150187, unit='angstrom')
         nuclei = mol.get_nuclei()
 
         cgfs = []
@@ -21,14 +21,14 @@ class TestCustomBasisSet(unittest.TestCase):
             cgfs.append(_cgf)
 
         res = HF(mol, basis=cgfs).rhf()
-        np.testing.assert_almost_equal(res['energy'], -1.1175059, 5)
+        np.testing.assert_almost_equal(res['energy'], -1.1175053456568793, 9)
 
     def test_custom_basis_set_co(self):
         mol = MoleculeBuilder.from_name('CO')
         cgfs, nuclei = mol.build_basis('sto3g')
 
         res = HF(mol, basis=cgfs).rhf()
-        np.testing.assert_almost_equal(res['energy'], -111.2192571, 4)
+        np.testing.assert_almost_equal(res['energy'], -111.21922241432557, 9)
 
 if __name__ == '__main__':
     unittest.main()

tests/test_hf.py
@@ -16,11 +16,11 @@ class TestHF(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -73.21447132, 4)
+        np.testing.assert_almost_equal(results['energy'], -73.21444239521301, 9)
 
         # verify that terms are being calculated
-        np.testing.assert_almost_equal(results['density'], np.einsum('ik,jk,k->ij', results['orbc'], results['orbc'], [2,2,2,2,2,0,0]), decimal=5)
-        np.testing.assert_almost_equal(results['ekin'] + results['enuc'] + results['erep'] + results['ex'] + results['enucrep'], results['energy'], decimal=5)
+        np.testing.assert_almost_equal(results['density'], np.einsum('ik,jk,k->ij', results['orbc'], results['orbc'], [2,2,2,2,2,0,0]), decimal=7)
+        np.testing.assert_almost_equal(results['ekin'] + results['enuc'] + results['erep'] + results['ex'] + results['enucrep'], results['energy'], decimal=7)
 
     def test_hartree_fock_ch4(self):
         """
@@ -37,20 +37,24 @@ class TestHF(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that orbital energies are correctly approximated
-        ans = np.array([-11.0707,
-                         -0.7392,
-                         -0.3752,
-                         -0.3752,
-                         -0.3752,
-                          0.2865,
-                          0.4092,
-                          0.4092,
-                          0.4092])
-        np.testing.assert_almost_equal(results['orbe'], ans, 4)
+        ans = np.array(
+            [
+                -11.070708604,
+                -0.739227472,
+                -0.37521838,
+                -0.37521838,
+                -0.37521838,
+                0.28650488,
+                0.40923474,
+                0.40923474,
+                0.40923474,
+            ]
+        )
+        np.testing.assert_almost_equal(results['orbe'], ans, 9)
 
-        en = -39.35007843284954
-        np.testing.assert_almost_equal(results['energies'][-1], en, 4)
-        
+        en = -39.35007280776424
+        np.testing.assert_almost_equal(results['energies'][-1], en, 9)
+
     def test_hartree_fock_ch4_symmetric(self):
         """
         Test Hartree-Fock calculation on CH4 using an STO-3g basis set and
@@ -67,19 +71,23 @@ class TestHF(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf(ortho='symmetric')
 
         # check that orbital energies are correctly approximated
-        ans = np.array([-11.0707,
-                         -0.7392,
-                         -0.3752,
-                         -0.3752,
-                         -0.3752,
-                          0.2865,
-                          0.4092,
-                          0.4092,
-                          0.4092])
-        np.testing.assert_almost_equal(results['orbe'], ans, 4)
+        ans = np.array(
+            [
+                -11.070708606,
+                -0.739227466,
+                -0.375218382,
+                -0.375218382,
+                -0.375218382,
+                0.286504886,
+                0.40923474,
+                0.40923474,
+                0.40923474,
+            ]
+        )
+        np.testing.assert_almost_equal(results['orbe'], ans, 9)
 
-        en = -39.35007843284954
-        np.testing.assert_almost_equal(results['energies'][-1], en, 4)
+        en = -39.35007280809286
+        np.testing.assert_almost_equal(results['energies'][-1], en, 9)
 
     def test_hartree_fock_restart(self):
         """
@@ -95,19 +103,23 @@ class TestHF(unittest.TestCase):
         results1 = HF(mol, 'sto3g').rhf()
 
         # check that orbital energies are correctly approximated
-        ans = np.array([-11.0707,
-                         -0.7392,
-                         -0.3752,
-                         -0.3752,
-                         -0.3752,
-                          0.2865,
-                          0.4092,
-                          0.4092,
-                          0.4092])
-        np.testing.assert_almost_equal(results1['orbe'], ans, 4)
+        ans = np.array(
+            [
+                -11.070708604,
+                -0.739227472,
+                -0.37521838,
+                -0.37521838,
+                -0.37521838,
+                0.28650488,
+                0.40923474,
+                0.40923474,
+                0.40923474,
+            ]
+        )
+        np.testing.assert_almost_equal(results1['orbe'], ans, 9)
 
-        en = -39.35007843284954
-        np.testing.assert_almost_equal(results1['energies'][-1], en, 4)
+        en = -39.35007280776424
+        np.testing.assert_almost_equal(results1['energies'][-1], en, 9)
 
         # create new CH4 molecule with slight adjustment in geometry and
         # seed the calculation with the previous converged result

tests/test_hf_molecules.py
@@ -7,7 +7,7 @@ class TestHFMolecules(unittest.TestCase):
     def testH2(self):
         """
         Test Hartree-Fock calculation for H2
-        
+
         Data is compared to results obtained from Gaussian
         """
         mol = Molecule()
@@ -18,11 +18,11 @@ class TestHFMolecules(unittest.TestCase):
 
         # check that energy matches
         np.testing.assert_almost_equal(results['energy'], -1.1175059, 5)
-    
+
     def testC2H4(self):
         """
         Test Hartree-Fock calculation for Ethylene
-        
+
         Data is compared to results obtained from Gaussian
         """
         mol = Molecule()
@@ -36,7 +36,7 @@ class TestHFMolecules(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -77.0739726, 4)
+        np.testing.assert_almost_equal(results['energy'], -77.07390203525122, 9)
 
     def testBF3(self):
         """
@@ -53,7 +53,7 @@ class TestHFMolecules(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -318.6619373, 4)
+        np.testing.assert_almost_equal(results['energy'], -318.66187490027113, 9)
 
     def testCH4(self):
         """
@@ -86,7 +86,7 @@ class TestHFMolecules(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -111.2254495, 4)
+        np.testing.assert_almost_equal(results['energy'], -111.22540898697989, 9)
 
     def testCO2(self):
         """
@@ -118,7 +118,7 @@ class TestHFMolecules(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -74.9659012, 4)
+        np.testing.assert_almost_equal(results['energy'], -74.96587833201312, 9)
 
     def testLIH(self):
         """
@@ -138,7 +138,7 @@ class TestHFMolecules(unittest.TestCase):
     def testNH3(self):
         """
         Test Hartree-Fock calculation for NH3
-        
+
         Note: a lower energy is found here as compared to Gaussian, so I reckon
         (might be wrong!) that this result is better.
         Using Gaussian: EHF = -55.7433617 Ht is found
@@ -153,12 +153,12 @@ class TestHFMolecules(unittest.TestCase):
         results = HF(mol, 'sto3g').rhf()
 
         # check that energy matches
-        np.testing.assert_almost_equal(results['energy'], -55.7998313, 4)
+        np.testing.assert_almost_equal(results['energy'], -55.79981538065868, 9)
 
     def testC6H6(self):
         """
         Test Hartree-Fock calculation for Ethylene
-        
+
         Data is compared to results obtained from Gaussian
         """
         mol = Molecule()

tests/test_kinetic.py
@@ -20,7 +20,7 @@ class TestKinetic(unittest.TestCase):
         gto3 = GTO(0.444635, [0.0, 0.0, 0.0], 0.168855, 0, 0, 0)
         kinetic = integrator.kinetic_gto(gto1, gto1)
         result = 1.595603108406067
-        np.testing.assert_almost_equal(kinetic, result, 4)
+        np.testing.assert_almost_equal(kinetic, result, 9)
 
     def test_cgf_kinetic(self):
         """
@@ -43,11 +43,11 @@ class TestKinetic(unittest.TestCase):
         T[0,1] = T[1,0] = integrator.kinetic(cgfs[0], cgfs[1])
         T[1,1] = integrator.kinetic(cgfs[1], cgfs[1])
 
-        T11 = 0.7600315809249878
-        T12 = 0.2364544570446014
-        np.testing.assert_almost_equal(T[0,0], T11, 4)
-        np.testing.assert_almost_equal(T[1,1], T11, 4)
-        np.testing.assert_almost_equal(T[0,1], T12, 4)
+        T11 = 0.7600315315362414
+        T12 = 0.23645443591648982
+        np.testing.assert_almost_equal(T[0,0], T11, 9)
+        np.testing.assert_almost_equal(T[1,1], T11, 9)
+        np.testing.assert_almost_equal(T[0,1], T12, 9)
 
 if __name__ == '__main__':
     unittest.main()

tests/test_nuclear.py
@@ -19,8 +19,8 @@ class TestNuclear(unittest.TestCase):
         gto2 = GTO(0.535328, [0.0, 0.0, 0.0], 0.623914, 0, 0, 0)
         gto3 = GTO(0.444635, [0.0, 0.0, 0.0], 0.168855, 0, 0, 0)
         nuclear = integrator.nuclear_gto(gto1, gto1, [0.0, 0.0, 1.0])
-        result = -0.31049036979675293
-        np.testing.assert_almost_equal(nuclear, result, 4)
+        result = -0.31049033047015934
+        np.testing.assert_almost_equal(nuclear, result, 9)
 
     def test_cgf_nuclear(self):
         """
@@ -47,15 +47,15 @@ class TestNuclear(unittest.TestCase):
         V2[0,1] = V2[1,0] = integrator.nuclear(cgfs[0], cgfs[1], cgfs[1].p, 1)
         V2[1,1] = integrator.nuclear(cgfs[1], cgfs[1], cgfs[1].p, 1)
 
-        V11 = -1.2266135215759277
-        V12 = -0.5974172949790955
-        V22 = -0.6538270711898804
-        np.testing.assert_almost_equal(V1[0,0], V11, 4)
-        np.testing.assert_almost_equal(V1[1,1], V22, 4)
-        np.testing.assert_almost_equal(V1[0,1], V12, 4)
-        np.testing.assert_almost_equal(V2[0,0], V22, 4)
-        np.testing.assert_almost_equal(V2[1,1], V11, 4)
-        np.testing.assert_almost_equal(V2[0,1], V12, 4)
+        V11 = -1.2266134491129144
+        V12 = -0.5974175949450241
+        V22 = -0.6538269055058872
+        np.testing.assert_almost_equal(V1[0,0], V11, 9)
+        np.testing.assert_almost_equal(V1[1,1], V22, 9)
+        np.testing.assert_almost_equal(V1[0,1], V12, 9)
+        np.testing.assert_almost_equal(V2[0,0], V22, 9)
+        np.testing.assert_almost_equal(V2[1,1], V11, 9)
+        np.testing.assert_almost_equal(V2[0,1], V12, 9)
 
 if __name__ == '__main__':
     unittest.main()

tests/test_plot_data.py
@@ -14,20 +14,20 @@ class TestPlotData(unittest.TestCase):
         mol.add_atom('H', 0.0, 0.00, -0.7)  # distances in Bohr lengths
         mol.add_atom('H', 0.0, 0.00, 0.7)   # distances in Bohr lengths
         cgfs, nuclei = mol.build_basis('sto3g')
-        
+
         # construct integrator object
         integrator = PyQInt()
-        
+
         # build grid points
         x = np.linspace(-2, 2, 6, endpoint=True)
         coord = np.flipud(np.vstack(np.meshgrid(x, x, x, indexing='ij')).reshape(3,-1)).T
-        
+
         c = np.array([0.54893397, 0.54893397])
         wf = integrator.plot_wavefunction(coord, c, cgfs)
-        
+
         res = np.load(os.path.join(os.path.dirname(__file__), 'results', 'h2_wf.npy'))
-        np.testing.assert_almost_equal(wf, res, 4)
-        
+        np.testing.assert_almost_equal(wf, res, 9)
+
     def test_plot_gradient(self):
         """
         Test generation of wave function
@@ -37,44 +37,49 @@ class TestPlotData(unittest.TestCase):
         mol.add_atom('H', 0.0, 0.00, -0.7)  # distances in Bohr lengths
         mol.add_atom('H', 0.0, 0.00, 0.7)   # distances in Bohr lengths
         cgfs, nuclei = mol.build_basis('sto3g')
-        
+
         # construct integrator object
         integrator = PyQInt()
-                       
+
         c = np.array([0.54893397, 0.54893397])
-        
+
         # test couple of single points
         sc = np.array([[-2.,-2.,-2.]])
         grad = integrator.plot_gradient(sc, c, cgfs)
         self.assertEqual(grad.shape[0], 1)
         self.assertEqual(grad.shape[1], 3)
-        res = np.array([[0.00926244,0.00926244,0.0076777]])
-        np.testing.assert_almost_equal(grad, res, 4)
-        
+        res = np.array([[0.009262441, 0.009262441, 0.007677702]])
+        np.testing.assert_almost_equal(grad, res, 9)
+
         sc = np.array([[-2., 2.,-2.]])
         grad = integrator.plot_gradient(sc, c, cgfs)
         self.assertEqual(grad.shape[0], 1)
         self.assertEqual(grad.shape[1], 3)
-        res = np.array([[0.00926244, -0.00926244, 0.0076777]])
-        np.testing.assert_almost_equal(grad, res, 4)
-        
+        res = np.array([[ 0.009262441, -0.009262441, 0.007677702]])
+        np.testing.assert_almost_equal(grad, res, 9)
+
         # test two points
         tc = np.array([[-2.,-2.,-2.],[-2., 2.,-2.]])
         grad = integrator.plot_gradient(tc, c, cgfs)
         self.assertEqual(grad.shape[0], 2)
         self.assertEqual(grad.shape[1], 3)
-        res = np.array([[0.00926244,0.00926244,0.0076777],[0.00926244, -0.00926244, 0.0076777]])
-        np.testing.assert_almost_equal(grad, res, 4)
-        
+        res = np.array(
+            [
+                [0.009262441, 0.009262441, 0.007677702],
+                [0.009262441, -0.009262441, 0.007677702],
+            ]
+        )
+        np.testing.assert_almost_equal(grad, res, 9)
+
         # test grid of points
         # x = np.linspace(-2, 2, 6, endpoint=True)
         # coord = np.flipud(np.vstack(np.meshgrid(x, x, x, indexing='ij')).reshape(3,-1)).T
         coord = integrator.build_rectgrid3d(-2, 2, 6, endpoint=True)
         grad = integrator.plot_gradient(coord, c, cgfs)
         self.assertEqual(grad.shape[0], 6*6*6)
-        self.assertEqual(grad.shape[1], 3)        
+        self.assertEqual(grad.shape[1], 3)
         res = np.load(os.path.join(os.path.dirname(__file__), 'results', 'h2_grad.npy'))
-        np.testing.assert_almost_equal(grad, res, 4)
+        np.testing.assert_almost_equal(grad, res, 9)
 
 if __name__ == '__main__':
     unittest.main()